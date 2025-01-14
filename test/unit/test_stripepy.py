8f30a783d6a5fd3de289e13fdd836edfe867416b
Bendik Berg <26233156+obikenobi23@users.noreply.github.com>
2025-01-14 17:13:39 +0100

    Add test logic for _check_neighborhood

--- test/unit/test_stripepy.py
+++ test/unit/test_stripepy.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 import scipy.sparse as ss
 
-from stripepy.stripepy import _log_transform, _band_extraction, _scale_Iproc, _extract_RoIs, _compute_global_pseudodistribution
+from stripepy.stripepy import _log_transform, _band_extraction, _scale_Iproc, _extract_RoIs, _compute_global_pseudodistribution, _check_neighborhood
 
 
 @pytest.mark.unit
@@ -117,6 +117,13 @@ class TestExtractRoIs:
 
 @pytest.mark.unit
 class TestComputeGlobalPseudodistribution:
+    #TODO: implement generator function for matrix
+    """
+    The input matrix looks like
+    |   9   0   0   |
+    |   0   2   0   |
+    |   0   4   0   |
+    """
     @pytest.mark.skip(reason="Outgoing matrix is not necessarily sparse")
     def test_is_sparse(self):
         I = ss.rand(10, 10, density=0.5, format="csr")
@@ -150,3 +157,122 @@ class TestComputeGlobalPseudodistribution:
         I_RoI = _compute_global_pseudodistribution(I, {"matrix": [3,3]})
 
         assert I_RoI.max() <= 1.0
+
+@pytest.mark.unit
+class TestCheckNeighborhood:
+    @pytest.fixture(scope="function")
+    def correct_mask(self):
+        yield [0] * 20 + [1] * 70 + [0] * 10
+    
+    @pytest.fixture(scope="function")
+    def generic_setup(self):
+        yield (
+            np.linspace(0.0, 29.0, 30), 
+            5.0, 
+            4, 
+            0.5
+            )
+    
+    @pytest.fixture(scope="class")
+    def values(self):
+        yield np.linspace(0.0, 99.0, 100)
+    
+    @pytest.fixture(scope="class")
+    def min_value(self):
+        yield 20.0
+    
+    @pytest.fixture(scope="class")
+    def neighborhood_size(self):
+        yield 10
+    
+    @pytest.fixture(scope="class")
+    def threshold_percentage(self):
+        yield 0.5
+    
+    #TODO: test_min_value_too_low_for_params and test_threshold_percentage_too_low modifies to the same list. Change this, so that every output is unique
+    def test_correct_output(self, values, min_value, neighborhood_size, threshold_percentage, correct_mask):
+
+        mask = _check_neighborhood(values, min_value, neighborhood_size, threshold_percentage)
+
+        assert mask == correct_mask
+    
+    def test_min_value_too_low_to_run(self, values, min_value, neighborhood_size, threshold_percentage, correct_mask):
+
+        with pytest.raises(AssertionError):
+            mask = _check_neighborhood(
+                values=values, 
+                min_value=-1.0, 
+                neighborhood_size=neighborhood_size, 
+                threshold_percentage=threshold_percentage)
+
+        
+        #assert mask == correct_mask
+
+    def test_min_value_too_low_for_params(self, values, min_value, neighborhood_size, threshold_percentage, correct_mask):
+        
+        mask = _check_neighborhood(
+            values=values, 
+            min_value=19.0, 
+            neighborhood_size=neighborhood_size, 
+            threshold_percentage=threshold_percentage)
+        correct_mask[19] = 1
+
+        assert mask == correct_mask
+
+    def test_min_value_too_large_for_params(self, values, min_value, neighborhood_size, threshold_percentage, correct_mask):
+        
+        mask = _check_neighborhood(
+            values=values, 
+            min_value=21.0, 
+            neighborhood_size=neighborhood_size, 
+            threshold_percentage=threshold_percentage)
+        correct_mask[20] = 0
+
+        assert mask == correct_mask
+
+    def test_neighborhood_size_too_low(self, values, min_value, neighborhood_size, threshold_percentage, correct_mask):
+
+        mask = _check_neighborhood(
+            values=values, 
+            min_value=min_value, 
+            neighborhood_size=9, 
+            threshold_percentage=threshold_percentage)
+        correct_mask[90] = 1
+
+        assert mask == correct_mask
+
+    def test_neighborhood_size_too_large(self, values, min_value, neighborhood_size, threshold_percentage, correct_mask):
+
+        mask = _check_neighborhood(
+            values=values, 
+            min_value=min_value, 
+            neighborhood_size=11, 
+            threshold_percentage=threshold_percentage)
+        correct_mask[89] = 0
+
+        assert mask == correct_mask
+
+    def test_threshold_percentage_too_low(self, values, min_value, neighborhood_size, threshold_percentage, correct_mask):
+
+        mask = _check_neighborhood(
+            values=values, 
+            min_value=min_value, 
+            neighborhood_size=neighborhood_size, 
+            threshold_percentage=0.4)
+        correct_mask[18] = 1
+        correct_mask[19] = 1
+
+        assert mask == correct_mask
+
+
+    def test_threshold_percentage_too_large(self, values, min_value, neighborhood_size, threshold_percentage, correct_mask):
+
+        mask = _check_neighborhood(
+            values=values, 
+            min_value=min_value, 
+            neighborhood_size=neighborhood_size, 
+            threshold_percentage=0.6)
+        correct_mask[20] = 0
+        correct_mask[21] = 0
+
+        assert mask == correct_mask
