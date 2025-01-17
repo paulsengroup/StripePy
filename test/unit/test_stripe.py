6018e22907089cda8d618ff8ad1cc05ca894f06d
Bendik Berg <26233156+obikenobi23@users.noreply.github.com>
2025-01-17 15:48:23 +0100

    Remove unnecessary test test_equal_coordinate

--- test/unit/test_stripe.py
+++ test/unit/test_stripe.py
@@ -2,31 +2,21 @@ import pytest
 
 from stripepy.utils.stripe import Stripe
 
+
 @pytest.fixture(scope="function")
 def U_stripe():
-    stripe = Stripe(
-        seed=5, 
-        top_pers=5.0, 
-        horizontal_bounds=(4,6), 
-        vertical_bounds=(0,4), 
-        where="upper_triangular"
-        )
+    stripe = Stripe(seed=5, top_pers=5.0, horizontal_bounds=(4, 6), vertical_bounds=(0, 4), where="upper_triangular")
     yield stripe
+
 
 @pytest.fixture(scope="function")
 def L_stripe():
-    stripe = Stripe(
-        seed = 5,
-        top_pers=5.0,
-        horizontal_bounds=(4,6),
-        vertical_bounds=(4,10),
-        where="lower_triangular"
-        )
+    stripe = Stripe(seed=5, top_pers=5.0, horizontal_bounds=(4, 6), vertical_bounds=(4, 10), where="lower_triangular")
     yield stripe
+
 
 @pytest.mark.unit
 class TestInferLocation:
-    @pytest.mark.skip(reason="Method not poperly implemented")
     def test_within_upper_triangle(self, U_stripe):
         UP = U_stripe._infer_location(U_stripe._seed, U_stripe._top_bound, U_stripe._bottom_bound)
 
@@ -37,20 +27,15 @@ class TestInferLocation:
 
         assert LO == "lower_triangular"
 
-    @pytest.mark.skip(reason="Method not poperly implemented")
-    def test_equal_coordinate(self, U_stripe):
-        with pytest.raises(Exception):
-            EQ = U_stripe._infer_location(10, 100, 100)
 
 @pytest.mark.unit
 class TestComputeConvexComp:
     def test_in_upper(self, U_stripe):
         comp = U_stripe._compute_convex_comp()
-        
+
         assert comp == 4
-    
+
     def test_in_lower(self, L_stripe):
         comp = L_stripe._compute_convex_comp()
 
         assert comp == 4
-
