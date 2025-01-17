b3022cf9af4088a69ef1366a4e621297544266d8
Bendik Berg <26233156+obikenobi23@users.noreply.github.com>
2025-01-17 16:31:51 +0100

    Add test logic for Stripe init

--- test/unit/test_stripe.py
+++ test/unit/test_stripe.py
@@ -16,6 +16,79 @@ def L_stripe():
 
 
 @pytest.mark.unit
+class TestInit:
+    def test_all_values_okay(self, U_stripe):
+        stripe = U_stripe
+
+        assert stripe._seed == 5
+        assert stripe._persistence == 5.0
+        assert stripe._where == "upper_triangular"
+        assert stripe._left_bound == 4
+        assert stripe._right_bound == 6
+        assert stripe._top_bound == 0
+        assert stripe._bottom_bound == 4
+        assert stripe._five_number == None
+        assert stripe._inner_mean == None
+        assert stripe._inner_std == None
+        assert stripe._outer_lmean == None
+        assert stripe._outer_rmean == None
+
+    def test_seed_too_low(self):
+        with pytest.raises(ValueError):
+            stripe = Stripe(
+                seed=-1, top_pers=5.0, horizontal_bounds=(4, 6), vertical_bounds=(0, 4), where="invalid location"
+            )
+
+    def test_top_pers_too_low(self):
+        with pytest.raises(ValueError):
+            stripe = Stripe(
+                seed=5, top_pers=-1.0, horizontal_bounds=(4, 6), vertical_bounds=(0, 4), where="upper_triangular"
+            )
+
+    def test_invalid_location(self):
+        with pytest.raises(ValueError):
+            stripe = Stripe(
+                seed=5, top_pers=5.0, horizontal_bounds=(4, 6), vertical_bounds=(0, 4), where="upper_triangular"
+            )
+
+    def test_left_bound_over_right(self):
+        with pytest.raises(ValueError):
+            stripe = Stripe(
+                seed=5, top_pers=5.0, horizontal_bounds=(7, 6), vertical_bounds=(0, 4), where="upper_triangular"
+            )
+
+    def test_left_bound_over_seed(self):
+        with pytest.raises(ValueError):
+            stripe = Stripe(
+                seed=5, top_pers=5.0, horizontal_bounds=(6, 6), vertical_bounds=(0, 4), where="upper_triangular"
+            )
+
+    def test_right_bound_over_left(self):
+        with pytest.raises(ValueError):
+            stripe = Stripe(
+                seed=5, top_pers=5.0, horizontal_bounds=(4, 3), vertical_bounds=(0, 4), where="upper_triangular"
+            )
+
+    def test_right_bound_over_seed(self):
+        with pytest.raises(ValueError):
+            stripe = Stripe(
+                seed=5, top_pers=5.0, horizontal_bounds=(4, 4), vertical_bounds=(0, 4), where="upper_triangular"
+            )
+
+    def test_top_bound_over_bottom(self):
+        with pytest.raises(ValueError):
+            stripe = Stripe(
+                seed=5, top_pers=5.0, horizontal_bounds=(4, 4), vertical_bounds=(1, 0), where="upper_triangular"
+            )
+
+    def test_bottom_bound_over_top(self):
+        with pytest.raises(ValueError):
+            stripe = Stripe(
+                seed=5, top_pers=5.0, horizontal_bounds=(4, 6), vertical_bounds=(5, 4), where="upper_triangular"
+            )
+
+
+@pytest.mark.unit
 class TestInferLocation:
     def test_within_upper_triangle(self, U_stripe):
         UP = U_stripe._infer_location(U_stripe._seed, U_stripe._top_bound, U_stripe._bottom_bound)
