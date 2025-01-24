80fff05d1e0b571676a0643a1b62bcc95c97249f
Bendik Berg <26233156+obikenobi23@users.noreply.github.com>
2025-01-24 15:45:55 +0100

    Remove tests for private methods

--- test/unit/test_stripe.py
+++ test/unit/test_stripe.py
@@ -1257,31 +1257,3 @@ class TestComputeBiodescriptors:
                 ),
             ):
                 assert stripe.five_number == None
-
-
-@pytest.mark.unit
-class TestInferLocation:
-    # TODO: If the tests are useful, move them to class TestSetBoundaryProperties. If not, delete.
-    def test_within_upper_triangle(self, U_stripe):
-        UP = U_stripe._infer_location(U_stripe._seed, U_stripe._top_bound, U_stripe._bottom_bound)
-
-        assert UP == "upper_triangular"
-
-    def test_within_lower_triangular(self, L_stripe):
-        LO = L_stripe._infer_location(L_stripe._seed, L_stripe._top_bound, L_stripe._bottom_bound)
-
-        assert LO == "lower_triangular"
-
-
-@pytest.mark.unit
-class TestComputeConvexComp:
-    # TODO: Delete.
-    def test_in_upper(self, U_stripe):
-        comp = U_stripe._compute_convex_comp()
-
-        assert comp == 4
-
-    def test_in_lower(self, L_stripe):
-        comp = L_stripe._compute_convex_comp()
-
-        assert comp == 4
