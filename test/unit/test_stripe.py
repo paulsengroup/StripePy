d160ffc25c1c9096ec0ce8fd369d9da1f0c7f8e3
Bendik Berg <26233156+obikenobi23@users.noreply.github.com>
2025-02-03 14:59:12 +0100

    Refactor test suite, remove irrelevant boundary test cases

--- test/unit/test_stripe.py
+++ test/unit/test_stripe.py
@@ -13,7 +13,24 @@ from stripepy.utils.stripe import Stripe
 
 @pytest.mark.unit
 class TestObjectInitialization:
-    def test_constructor(self):
+    def test_seed_outside_matrix(self):
+        with pytest.raises(ValueError, match="seed must be a non-negative integral number"):
+            stripe = Stripe(seed=-1, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
+
+    def test_top_persistence_negative(self):
+        with pytest.raises(ValueError, match="when not None, top_pers must be a positive number"):
+            stripe = Stripe(seed=0, top_pers=-1.0, horizontal_bounds=None, vertical_bounds=None, where=None)
+
+    def test_where_invalid_input(self):
+        with pytest.raises(ValueError, match="when specified, where must be one of (.*upper.*|.*lower.*){2}"):
+            stripe = Stripe(
+                seed=5, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where="invalid_triangular"
+            )
+
+
+@pytest.mark.unit
+class TestBoundaryGetters:
+    def test_valid_access(self):
         stripe = Stripe(
             seed=5, top_pers=5.0, horizontal_bounds=(4, 6), vertical_bounds=(1, 4), where="upper_triangular"
         )
@@ -26,7 +43,14 @@ class TestObjectInitialization:
         assert stripe.top_bound == 1
         assert stripe.bottom_bound == 4
 
-    def test_none_to_constructor(self):
+        stripe = Stripe(
+            seed=5, top_pers=5.0, horizontal_bounds=(4, 6), vertical_bounds=(4, 10), where="lower_triangular"
+        )
+
+        assert stripe.lower_triangular
+        assert not stripe.upper_triangular
+
+    def test_not_valid_access(self):
         stripe = Stripe(seed=5, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
 
         assert stripe.seed == 5
@@ -44,50 +68,7 @@ class TestObjectInitialization:
 
 
 @pytest.mark.unit
-class TestSeed:
-    def test_seed_at_matrix_border(self):
-        stripe = Stripe(seed=0, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
-
-        assert stripe.seed == 0
-
-    def test_seed_outside_matrix(self):
-        with pytest.raises(ValueError, match="seed must be a non-negative integral number"):
-            stripe = Stripe(seed=-1, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
-
-    def test_seed_none(self):
-        with pytest.raises(TypeError, match="'<' not supported between instances of 'NoneType' and 'int'"):
-            stripe = Stripe(
-                seed=None, top_pers=5.0, horizontal_bounds=(4, 6), vertical_bounds=(1, 4), where="upper_triangular"
-            )
-
-    def test_seed_higher_value(self):
-        stripe = Stripe(seed=100, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
-
-
-@pytest.mark.unit
-class TestTopPersistence:
-    def test_top_persistence_1(self):
-        stripe = Stripe(seed=0, top_pers=1.0, horizontal_bounds=None, vertical_bounds=None, where=None)
-
-        assert np.isclose(stripe.top_persistence, 1.0)
-
-    def test_top_persistence_0(self):
-        stripe = Stripe(seed=0, top_pers=0.0, horizontal_bounds=None, vertical_bounds=None, where=None)
-
-        assert np.isclose(stripe.top_persistence, 0.0)
-
-    def test_top_persistence_negative(self):
-        with pytest.raises(ValueError, match="when not None, top_pers must be a positive number"):
-            stripe = Stripe(seed=0, top_pers=-1.0, horizontal_bounds=None, vertical_bounds=None, where=None)
-
-    def test_top_persistence_higher_value(self):
-        stripe = Stripe(seed=0, top_pers=100.0, horizontal_bounds=None, vertical_bounds=None, where=None)
-
-        assert np.isclose(stripe.top_persistence, 100.0)
-
-
-@pytest.mark.unit
-class TestHorizontalBoundaryValues:
+class TestBoundarySetters:
     def test_left_and_right_at_seed(self):
         stripe = Stripe(seed=5, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
 
@@ -96,37 +77,26 @@ class TestHorizontalBoundaryValues:
         assert stripe.left_bound == 5
         assert stripe.right_bound == 5
 
-    def test_seed_to_left_of_boundary(self):
+    def test_seed_outside_horizontal_bounds(self):
         stripe = Stripe(seed=5, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
         with pytest.raises(
             ValueError, match="horizontal bounds must enclose the seed position: seed=5, left_bound=6, right_bound=6"
         ):
             stripe.set_horizontal_bounds(6, 6)
-
-    def test_seed_to_right_of_boundary(self):
-        stripe = Stripe(seed=5, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
         with pytest.raises(
             ValueError,
             match="horizontal bounds must enclose the seed position: seed=5, left_bound=4, right_bound=4",
         ):
             stripe.set_horizontal_bounds(4, 4)
 
-    def test_left_and_right_cross_themselves(self):
+    def test_empty_horizontal_domains(self):
         stripe = Stripe(seed=5, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
         with pytest.raises(
             ValueError, match="horizontal bounds must enclose the seed position: seed=5, left_bound=6, right_bound=5"
         ):
             stripe.set_horizontal_bounds(6, 5)
 
-    def test_left_and_right_at_matrix_edge(self):
-        stripe = Stripe(seed=0, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
-
-        stripe.set_horizontal_bounds(0, 0)
-
-        assert stripe.left_bound == 0
-        assert stripe.right_bound == 0
-
-    def test_left_over_matrix_edge(self):
+    def test_negative_horizontal_bounds(self):
         stripe = Stripe(seed=1, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
         with pytest.raises(ValueError, match="stripe bounds must be positive integers"):
             stripe.set_horizontal_bounds(-1, 1)
@@ -137,9 +107,6 @@ class TestHorizontalBoundaryValues:
         with pytest.raises(RuntimeError, match="horizontal stripe bounds have already been set"):
             stripe.set_horizontal_bounds(5, 7)
 
-
-@pytest.mark.unit
-class TestVerticalBoundaryValues:
     def test_vertical_size_0(self):
         stripe = Stripe(seed=5, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
 
@@ -148,7 +115,7 @@ class TestVerticalBoundaryValues:
         assert stripe.top_bound == 5
         assert stripe.bottom_bound == 5
 
-    def test_top_and_bottom_cross_themselves(self):
+    def test_empty_vertical_domains(self):
         stripe = Stripe(seed=5, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
 
         with pytest.raises(
@@ -157,15 +124,7 @@ class TestVerticalBoundaryValues:
         ):
             stripe.set_vertical_bounds(5, 4)
 
-    def test_top_and_bottom_at_matrix_edge(self):
-        stripe = Stripe(seed=0, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
-
-        stripe.set_vertical_bounds(0, 0)
-
-        assert stripe.top_bound == 0
-        assert stripe.bottom_bound == 0
-
-    def test_top_and_bottom_over_matrix_edge(self):
+    def test_negative_vertical_bounds(self):
         stripe = Stripe(seed=5, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where=None)
         with pytest.raises(ValueError, match="stripe bounds must be positive integers"):
             stripe.set_vertical_bounds(-1, -1)
@@ -175,15 +134,6 @@ class TestVerticalBoundaryValues:
 
         with pytest.raises(RuntimeError, match="vertical stripe bounds have already been set"):
             stripe.set_vertical_bounds(2, 6)
-
-
-@pytest.mark.unit
-class TestWhere:
-    def test_where_invalid_input(self):
-        with pytest.raises(ValueError, match="when specified, where must be one of (.*upper.*|.*lower.*){2}"):
-            stripe = Stripe(
-                seed=5, top_pers=None, horizontal_bounds=None, vertical_bounds=None, where="invalid_triangular"
-            )
 
 
 @pytest.mark.unit
